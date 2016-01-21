5478fa2fbff66d52aae620526fbcbc1d5a8eb4e7
Atabek <atabekmur@gmail.com>
2016-01-21 23:02:16 +1100

    Add basic routing

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 var express = require('express');
+var path = require('path');
+
+var index = require('./routes/index')
 
 var app = express();
 
-app.get('/', function(request, response) {
-	response.end("Hello World");
-});
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// routes
+app.use('/', index);
 
 app.listen(3000);
 console.log("Server started");
