5706a2f1aeb77bbd7b852a5fe84d430c5feca650
Atabek <atabekmur@gmail.com>
2016-01-23 13:08:36 +1100

    Add template header, footer, and styling

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home page', content: 'Welcome to simple analytics project' });
+  res.render('index');
 });
 
 module.exports = router;
